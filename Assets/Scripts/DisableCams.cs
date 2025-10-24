8b18914a65193756a3348d255ad653799cbcd14c
Bucket <lennprinz@gmail.com>
2025-10-24 16:40:17 +0200

    Tweaks for prototype

--- Assets/Scripts/DisableCams.cs
+++ Assets/Scripts/DisableCams.cs
@@ -6,7 +6,7 @@ public class DisableCams : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        SwitchEm();
     }
 
     private void OnTriggerEnter(Collider other)
