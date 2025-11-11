bdbb420f36e68fcb42346329a0467d3ebc50c007
Bucket <lennprinz@gmail.com>
2025-11-11 16:05:42 +0100

    Movable PC windows, energy window prototype
    - Click and drag to move windows
    - Clicking a window will make it render above the rest
    - Window displaying energy status has been added as a prototype
    - Known bug: dragging a window over another blocks the lower window from being moved, still looking into this

---
+++ Assets/Scripts/PcWindow.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class PcWindow : MonoBehaviour
+{
+    MonitorCursor cursor;
+    private bool isGrabbed;
+    PcWindow overlappedWindow;
+    private void Update()
+    {
+        if (cursor != null)
+        {
+            if (cursor.beingControlled)
+            {
+                if (Input.GetKeyDown(KeyCode.Mouse0))
+                {
+                    if (overlappedWindow != null)
+                    {
+                        if (transform.GetSiblingIndex() > overlappedWindow.transform.GetSiblingIndex())
+                        {
+                            isGrabbed = true;
+                        }
+                    }
+                    else isGrabbed = true;
+                }
+                if (Input.GetKey(KeyCode.Mouse0) && isGrabbed)
+                {
+                    transform.SetAsLastSibling();
+                    transform.SetSiblingIndex(transform.GetSiblingIndex() - 1);
+                    transform.localPosition += new Vector3(Input.GetAxis("Mouse X") * cursor.cursorSpeed, Input.GetAxis("Mouse Y") * cursor.cursorSpeed, 0f);
+
+                }
+                if (Input.GetKeyUp(KeyCode.Mouse0))
+                    isGrabbed = false;
+            }
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.GetComponent<MonitorCursor>() != null)
+        {
+            cursor = collision.GetComponent<MonitorCursor>();
+        }
+        if (collision.GetComponent<PcWindow>())
+        {
+            Debug.Log("Colliding with" + collision.name);
+            overlappedWindow = collision.GetComponent<PcWindow>();
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.GetComponent<MonitorCursor>())
+            cursor = null;
+        if (collision.GetComponent<PcWindow>())
+        {
+            overlappedWindow = null;
+        }
+    }
+}
