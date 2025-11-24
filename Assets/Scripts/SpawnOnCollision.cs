d21dbe1fdc1f2544ac3b28fd4de36fdebf0b98ee
Bucket <lennprinz@gmail.com>
2025-11-24 15:49:15 +0100

    Post processing changes & fixes + SpawnOnCollision improvements
    - Depth of field effect added back into the sanity changes, depth of field is now also present in the regular post processing but to a lesser degree to improve the transition between the two
    - SpawnOnCollision will now check if the thing to spawn is already active in the hierarchy upon loading the scene. If it is, it'll be disabled until it gets spawned upon collision.

--- Assets/Scripts/SpawnOnCollision.cs
+++ Assets/Scripts/SpawnOnCollision.cs
@@ -4,25 +4,44 @@ public class SpawnOnCollision : MonoBehaviour
 {
     [SerializeField] GameObject objectToSpawn;
     [SerializeField] Transform objectSpawnTransform;
+    [SerializeField] float sanityThreshold = 100f;
 
-    [SerializeField] enum SpawnType
+    [SerializeField]
+    enum SpawnType
     {
         InstanciateObject, EnableExisting
     }
     [SerializeField] SpawnType _type;
+    private void Start()
+    {
+        if (objectToSpawn != null)
+        {
+            if (objectToSpawn.activeInHierarchy)
+            {
+                objectToSpawn.SetActive(false);
+            }
+        }
+    }
 
     // Update is called once per frame
     private void OnTriggerEnter(Collider other)
     {
-        if (objectToSpawn != null) 
+        if (other.CompareTag("Player"))
         {
-            if (other.CompareTag("Player"))
+            if (objectToSpawn != null)
             {
-                if (_type == SpawnType.InstanciateObject)
-                Instantiate(objectToSpawn, objectSpawnTransform.position, objectSpawnTransform.rotation);
-                if (_type == SpawnType.EnableExisting)
-                    objectToSpawn.SetActive(true);
-                Destroy(gameObject);
+                if (other.GetComponent<PlayerStats>().Sanity <= sanityThreshold)
+                {
+                    if (_type == SpawnType.InstanciateObject)
+                        Instantiate(objectToSpawn, objectSpawnTransform.position, objectSpawnTransform.rotation);
+                    if (_type == SpawnType.EnableExisting)
+                        objectToSpawn.SetActive(true);
+                    Destroy(gameObject);
+                }
+            }
+            else
+            {
+                Debug.LogError("[SpawnOnCollision] Could not spawn object: No object specified");
             }
         }
     }
