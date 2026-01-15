6e6163c4bb0f9abd3fa18226c11d5a13ee5f10de
Bucket <lennprinz@gmail.com>
2026-01-15 12:59:05 +0100

    Cornerillusion Jumpscare sounds
    - Added some sounds to the cornerillusion
    - Adjusted AnimateOnCollision to allow for playing sounds
    - Renamed Endingsounds folder to Ending

--- Assets/Scripts/Hallucinations/AnimateOnCollision.cs
+++ Assets/Scripts/Hallucinations/AnimateOnCollision.cs
@@ -3,12 +3,17 @@ using UnityEngine;
 public class AnimateOnCollision : MonoBehaviour
 {
     [SerializeField] Animator animator;
-
+    [SerializeField] bool playSound;
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip[] clips;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             animator.SetTrigger("play");
+            if (playSound)
+                audioSource.PlayOneShot(clips[Random.Range(0, clips.Length)]);
+            GetComponent<BoxCollider>().enabled = false;
         }
     }
 }
