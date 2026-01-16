580898bc7f97877f7ca87a8811a7d0349a3c029b
AuraAmile <dave.v.staden@gmail.com>
2026-01-16 13:33:46 +0100

    Cat Fix

---
+++ Assets/Scripts/AudioScripts/CatMauwSound.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class CatMauwSound : MonoBehaviour
+{
+    private float time;
+    private float maxTime = 3f;
+    bool chase = false;
+    public AudioSource mauwSource;
+    void Start()
+    {
+        time = maxTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (chase)
+        {
+            time -= Time.deltaTime;
+            if (time <= 0f)
+            {
+                mauwSource.Play();
+                time = maxTime;
+            }
+        }
+    }
+
+    void startChase()
+    {
+        chase = true;
+    }
+}
